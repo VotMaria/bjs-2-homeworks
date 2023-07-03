ae65eb8cb4af4adc1c36103dd80e47ad21a32d03
VotMaria <votmariya@yandex.ru>
2023-07-03 17:08:23 +0300

    Исправлено 3 задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,17 @@
-function compareArrays(arr1, arr2){
+/*function compareArrays(arr1, arr2){
    if (arr1.length === arr2.length && arr1.every((element, index) =>  element === arr2[index])) {
       return true;
    } else {
       return false;
    }  
-}
-    
+}*/
+   
+
+function compareArrays(arr1, arr2){
+   return (arr1.length === arr2.length && arr1.every((element, index) =>  element === arr2[index]))
+  }
 
 
 function getUsersNamesInAgeRange(users, gender) {
-   let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, users) => acc + item / users.length, 0)
-   return result;
+   return result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, users) => acc + item / users.length, 0)
 }
