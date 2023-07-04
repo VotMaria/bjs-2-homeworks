0515b591486f6cb220987ec1c27e470bb70a3243
VotMaria <votmariya@yandex.ru>
2023-07-04 23:11:23 +0300

    Добавлено исправление

--- 7.async/task.js
+++ 7.async/task.js
@@ -21,6 +21,8 @@ class AlarmClock {
         let date = new Date();
         let hours = date.getHours();
         let minutes = date.getMinutes();
+        hours = (hours < 10) ? '0' + hours : hours;
+        minutes = (minutes < 10) ? '0' + minutes : minutes;
         return hours + ':' + minutes;
     }
 
