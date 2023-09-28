08ba61c11e92f47e6ce1cfb0a6ad1afaeb14f83c
VotMaria <votmariya@yandex.ru>
2023-09-28 12:39:44 +0300

    arrays_ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -5,13 +5,13 @@
       return false;
    }  
 }*/
-   
 
-function compareArrays(arr1, arr2){
-   return (arr1.length === arr2.length && arr1.every((element, index) =>  element === arr2[index]))
-  }
+
+function compareArrays(arr1, arr2) {
+	return (arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]))
+}
 
 
 function getUsersNamesInAgeRange(users, gender) {
-   return result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, users) => acc + item / users.length, 0)
+	return result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, users) => acc + item / users.length, 0)
 }
